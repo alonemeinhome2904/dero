9899990cb146f012bb305709aeee7c323aa78d20
alonemeinhome2904 <118436537+alonemeinhome2904@users.noreply.github.com>
2023-03-12 02:26:11 +0700

    Update stratum_mining.sh

--- stratum_mining.sh
+++ stratum_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    ./astrominer -w dero1qy5ehjh9ztymrket2le2g7sjny2584m4wyv4dqh6t7sfuhwkj9v8qqgdxmp9c -r stratum+tls://dero.friendspool.club:10300 -p stratum;
     sleep 5;
 done
